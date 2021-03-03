ac622056e24e7a5ccecf7a6004a364d032f9bfe1
Yury Shashurin <shashurin.edu@mail.ru>
2021-03-03 23:44:49 +0300

    edit setting, add dotenv

--- Cars/settings.py
+++ Cars/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
